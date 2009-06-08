362772f1abcbf8e8a7a56738c95e0b31d121cfb5
Andrey Sinicin <andrey.sinicin@dalee.ru>
2009-06-08 11:38:38 +0000

    Some fixes for cronrun

--- sbin/cronRun.php
+++ sbin/cronRun.php
@@ -5,8 +5,9 @@ ini_set('memory_limit','512M'); //for greedy scripts
 require_once dirname(__FILE__).'/../lib/maincommon.inc';
 
 $engine = new PXEngineSbin();
-$engine->init();
+
 $app =& $engine->app;
+
 if (!isset($app->modules['cronrun'])) return;
 
 $cronModule = $app->modules['cronrun']->getModule();
@@ -23,6 +24,13 @@ if (isset($cronModule->jobs[$jobName])) {
 
 } else {
 	ini_set('display_errors', 0);
+	
+	echo "\n";
+	foreach($cronModule->jobs as $jobName => $j) {
+			echo "\t".str_pad($jobName, 25).str_pad($j['rule']->asString, 25).$j['job']->name."\n";
+	}
+	echo "\n";
+
 	Label('Run all jobs, if it\'s time');
 
 	$cronModule->RunTasks($app, time());
