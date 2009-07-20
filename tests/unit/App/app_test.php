f5694e635c17f5a16d54e58cb78b5ec86b068578
Vitaly Katorgin <vitalykatorgin@gmail.com>
2009-07-20 19:00:47 +0000

    some change in app and 's test

--- tests/unit/App/app_test.php
+++ tests/unit/App/app_test.php
@@ -1,100 +1,59 @@
 <?php
 
-/*{{{
-	<datatype name="suser" description="Пользователь" orderby="title" parent="sgroup" bydefault="all">
-		<attribute name="id"          description="ID"                  storagetype="pk"          displaytype="HIDDEN"/>
-		<attribute name="parent"      description="Входит в группу"     storagetype="integer"     displaytype="PARENTDROPDOWN" source="sgroup" listed="true"/>
-		<attribute name="title"       description="Логин"               storagetype="string"      displaytype="TEXT"/>
-		<attribute name="passwd"      description="Пароль"              storagetype="string"      displaytype="PASSWORD" listed="true"/>
-		<attribute name="realname"    description="Имя"                 storagetype="string"      displaytype="TEXT"/>
-		<attribute name="email"       description="E-mail"              storagetype="string"      displaytype="TEXT"/>
-		<attribute name="access"      description="Доступ"              storagetype="integer"     displaytype="DROPDOWN"  listed="true" source="suser-access"/>
-		<attribute name="data"        description="MISC"                storagetype="serialized"  displaytype="STATIC"/>
-		<attribute name="status"      description="Активен"             storagetype="boolean"     displaytype="CHECKBOX"/>
-	</datatype>
-}}}*/
-
 class AppTest extends UnitTestCase {
 	function setUp() {
-		$e = new PXEngineSbin();
-		$this->app = PXRegistry::getApp();
-	}
-
-	/**
-	 * Проверить структуру массива 
-	 * проверить структуру элемента
-	 **/
-	function checkStruct($in, $ext) {
-		$this->assertIsA($in, "array");
-
-		$map = array_keys(get_object_vars($ext));
-		sort($map);
-
-		foreach($in as $i) {
-			$this->assertIsA($i, get_class($ext));
-
-			$ex1 = array_keys(get_object_vars($i));
-			sort($ex1);
-
-			$this->assertIdentical($ex1, $map);
-		}
-	}
-
-	function testTypes() {
-		$this->checkStruct($this->app->types, new PXTypeDescription());
+		$this->app = PXApplication::getInstance("PXEngineIndex");
 	}
 	
-	/*function testDescrpition() {
-		$this->checkStruct($this->app->dbDescription, new NLDbDescription());
-	}*/
+	function testGetInstance() {
+		$engines  = array("sbin", "json", "sbin", "action", "adminAction", "adminIndex", "adminPopup");
 
-	function testModules() {
-		$this->checkStruct($this->app->modules, new PXModuleDescription());
+		while($e  = array_shift($engines)) 
+			$this->assertIsA(PXApplication::getInstance("PXEngine".ucfirst($e)), "PXApplication");
+
 	}
 
-	function testDirectory() {
-		$this->checkStruct($this->app->directory, new PXDirectoryDescription("suser"));
+	function testParseProperties() {
 	}
 
-    /*
-    FIXME: Rwrite test
-
-    function testModBindings() {
-		$ext = array("type", "var", "value", "module", "order", "pOrder");
-		sort($ext);
-        $bids = $this->app->bindingsQueue;
-        
-        $this->assertIsA($bids, "PXBindingsQueue");
-
-		foreach($bids as $i) {
-			$md_k = array_keys($i);
-			sort($md_k);
-			$this->assertIdentical($md_k, $ext);
+	function testGetAvailableModules() {
+		foreach($this->app->getAvailableModules(16384) as $k=>$v) {
+			$this->assertIsA($v, "PXModuleDescription");
 		}
-    }
-    */
+	}
 
-	function testGetProperty() {
-		$pone  = $this->app->getProperty("NON_EXISTING_TEST_PROPERTY",  1);
-        $pzero = $this->app->getProperty("EXISTING_TEST_PROPERTY0", "**FAILURE**");
+	function testGetProperty() {/*{{{*/
+		$app = PXApplication::getInstance("PXEngineSbin");
+		
+		$pone  = $app->getProperty("NON_EXISTING_TEST_PROPERTY",  1);
+		$pzero = $app->getProperty("EXISTING_TEST_PROPERTY0", "**FAILURE**");
+		
 		$this->assertEqual($pone, 1);
 		$this->assertEqual($pzero, 0);
-	}
+	}/*}}}*/
 
-	function testInitContentObject() {
-		$r = $this->app->initContentObject("suser");
+	function testInitContentObject() {/*{{{*/
+		$app = PXApplication::getInstance("PXEngineSbin");
+		
+		$r = $app->initContentObject("suser");
+		
+		/* {{{ <datatype name="suser" description="Пользователь" orderby="title" parent="sgroup" bydefault="all">
+			<attribute name="id"          description="ID"                  storagetype="pk"          displaytype="HIDDEN"/>
+			<attribute name="parent"      description="Входит в группу"     storagetype="integer"     displaytype="PARENTDROPDOWN" source="sgroup" listed="true"/>
+			<attribute name="title"       description="Логин"               storagetype="string"      displaytype="TEXT"/>
+			<attribute name="passwd"      description="Пароль"              storagetype="string"      displaytype="PASSWORD" listed="true"/>
+			<attribute name="realname"    description="Имя"                 storagetype="string"      displaytype="TEXT"/>
+			<attribute name="email"       description="E-mail"              storagetype="string"      displaytype="TEXT"/>
+			<attribute name="access"      description="Доступ"              storagetype="integer"     displaytype="DROPDOWN"  listed="true" source="suser-access"/>
+			<attribute name="data"        description="MISC"                storagetype="serialized"  displaytype="STATIC"/>
+			<attribute name="status"      description="Активен"             storagetype="boolean"     displaytype="CHECKBOX"/>
+			</datatype> }}} */
 		
 		$expected= array_combine($k = array("id", "parent", "title", "passwd", "realname", "email", "access", "data", "status"), 
 			array_pad(array(), sizeof($k), null));
 
 		$this->assertIdentical($r, $expected);
-	}
-	
-	function testGetAvailableModules() {
-		//admin
-		$r = $this->app->getAvailableModules(16384);
-		$this->checkStruct($r, new PXModuleDescription());
-	}
-}	
+	}/*}}}*/
+}
 
 ?>
