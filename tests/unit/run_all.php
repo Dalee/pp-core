59c2e0c9295e078a554424863af272ce3316a0d0
Andrey Nikolaev <andrey.nikolaev@dalee.ru>
2009-07-20 14:30:48 +0000

    Run all or run one.

--- tests/unit/run_all.php
+++ tests/unit/run_all.php
@@ -7,22 +7,23 @@
 	require_once BASEPATH . '/libpp/lib/mainadmin.inc';
 	require_once BASEPATH . '/libpp/lib/maincommon.inc';
 	
+	#require_once BASEPATH . '/libpp/vendor/simpletest/autorun.php';
 	require_once BASEPATH . '/libpp/vendor/simpletest/unit_tester.php';
 	require_once BASEPATH . '/libpp/vendor/simpletest/reporter.php';
 	require_once BASEPATH . '/libpp/vendor/simpletest/collector.php';
 	require_once BASEPATH . '/libpp/vendor/simpletest/mock_objects.php';
 	
-	//require_once BASEPATH . '/libpp/tests/unit/DisplayType/classes.php';
-	//require_once BASEPATH . '/libpp/tests/unit/StorageType/classes.php';
-
 	/* Please do includes in your own tests. */
-	$current_dir = BASEPATH . '/libpp/tests/unit';
+	$unit_tests_dir = BASEPATH . '/libpp/tests/unit';
 
-	$tests =& new TestSuite('libpp tests');
+	$tests = new TestSuite('libpp tests');
 	
-	//for tested some dir
-	$once = isset($_SERVER['argv'][1]) ? $_SERVER['argv'][1] : "";
+	$test_single_file = isset($_SERVER['argv'][1]) ? $_SERVER['argv'][1] : NULL;
+    
+    if ($test_single_file)
+        $tests->addFile(realpath($unit_tests_dir . '/' . $test_single_file));
+    else
+	    $tests->collect($unit_tests_dir, new RecursivePatternCollector("#_test.php$#i"));
 
-	$tests->collect($current_dir, new RecursivePatternCollector("#{$once}_test.php$#i"));
 	$tests->run(new TextReporter());
 ?>
