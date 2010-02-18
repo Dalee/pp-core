4267de8b1ac9965bc3f6ecc7a794a64c5d3d0569
Anna Andreeva <anna.andreeva@dalee.ru>
2010-02-18 12:01:30 +0000

    cronrun fix

--- sbin/cronRun.php
+++ sbin/cronRun.php
@@ -18,24 +18,30 @@ if (!isset($app->modules['cronrun'])) {
 $cronModule = $app->modules['cronrun']->getModule();
 $jobName    = isset($_SERVER['argv'][1]) ? $_SERVER['argv'][1] : NULL;
 
-if (isset($cronModule->jobs[$jobName])) {
+if($jobName) {
 	ini_set('display_errors', 1);
-	Label('Run: '.$jobName);
 
-	$cronModule->runJob($cronModule->jobs[$jobName], $app, time());
+	if (isset($cronModule->jobs[$jobName])) {
+		Label('Run: '.$jobName);
 
-} else {
-	ini_set('display_errors', 0);
-	Label('Choose job:');
+		$cronModule->runJob($cronModule->jobs[$jobName], $engine->app, time());
 
-	echo "\n";
-	foreach($cronModule->jobs as $jobName => $j) {
-		echo "\t".str_pad($jobName, 25).str_pad($j['rule']->asString, 25).$j['job']->name."\n";
+	} else {
+		Label('Choose job:');
+		echo "\n";
+
+		foreach($cronModule->jobs as $jobName => $j) {
+			echo "\t".str_pad($jobName, 25).str_pad($j['rule']->asString, 25).$j['job']->name."\n";
+		}
+		
+		echo "\n";
 	}
-	echo "\n";
 
-	Label('Run all jobs, if it\'s time');
+	} else {
+		ini_set('display_errors', 0);
+		Label('Run all jobs, if it\'s time');
 
-	$cronModule->RunTasks($app, time());
+		$cronModule->RunTasks($engine->app, time());
+	}
 }
 ?>
