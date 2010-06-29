fd9b29b717ea01b11c3d6b47b14bae4414527dbd
Vitaly Katorgin <vitalykatorgin@gmail.com>
2010-06-29 14:27:42 +0000

    merge with php5-loader-implant@32462

--- plugins/contentversion/alterpg.php
+++ plugins/contentversion/alterpg.php
@@ -1,5 +1,6 @@
 #!/usr/bin/php5 -q
 <?php
+
 require_once dirname(__FILE__).'/../../lib/maincommon.inc';
 
 $engine = new PXEngineSbin();
--- plugins/contentversion/decorators.php
+++ plugins/contentversion/decorators.php
@@ -1,18 +1,31 @@
 <?php
-require_once(BASEPATH . '/libpp/plugins/contentversion/widget.class.inc');
  
 PXDecorativeWidgetsCollection::addToCollection(
-	new PXVersion,
+	new PXVersion('content'),
 	'PXAdminForm',
 	'VERSION_CONTENT',
 	PXAdminForm::CONTENT
 );
 
-/*PXDecorativeWidgetsCollection::addToCollection(
-	new ,
+PXDecorativeWidgetsCollection::addToCollection(
+	new PXVersion('leftcontrols'),
 	'PXAdminForm',
 	'VERSION_CONTENT',
-	PXAdminForm::CONTENT
-);*/
+	PXAdminForm::LEFTCONTROLS
+);
+
+PXDecorativeWidgetsCollection::addToCollection(
+	new PXVersion('title'),
+	'PXAdminForm',
+	'VERSION_CONTENT',
+	PXAdminForm::TITLE
+);
+
+PXDecorativeWidgetsCollection::addToCollection(
+	new PXVersionsTab,
+	'PXWidgetTabs',
+	'VERSION_CONTENT',
+	PXWidgetTabs::APPEND
+);
 
 ?>
