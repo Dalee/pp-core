1dacd99dedb623640f139a9580082b260ff5bee2
Stanislav Tugovikov <stanislav.tugovikov@dalee.ru>
2010-07-26 06:18:30 +0000

    fix code style

--- plugins/multipleregions/sbin/addReflexId.php
+++ plugins/multipleregions/sbin/addReflexId.php
@@ -5,18 +5,20 @@ ini_set('memory_limit','512M'); //for greedy scripts
 require_once dirname(__FILE__).'/../../../lib/maincommon.inc';
 
 $engine = new PXEngineSbin();
-$app = PXRegistry::getApp();
 $db  = PXRegistry::getDB();
 
-$r = $db->query("select table_name from information_schema.columns where column_name = 'sys_regions'");
+$r = $db->query("SELECT table_name FROM information_schema.columns WHERE column_name = 'sys_regions'");
 
-if(empty($r)) return;
+if(empty($r)) {
+	return;
+}
 
 $r = array_map("pos", $r);
 
 foreach($r as $i) {
 	$db->transactionBegin();
-	$db->modifyingQuery(sprintf("alter table %s add reflex_id int, add deny_region_edit bool", $i));
+	Label($i);
+	$db->modifyingQuery(sprintf("ALTER TABLE %s ADD reflex_id INT, ADD deny_region_edit BOOL", $i));
 	$db->transactionCommit();
 }
 
