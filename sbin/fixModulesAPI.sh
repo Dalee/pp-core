effb7c78e4c27394e6fd216ba657f5fad1cef069
Vitaly Katorgin <vitalykatorgin@gmail.com>
2010-08-19 10:47:01 +0000

    fix fixModulesApi.sh

--- sbin/fixModulesAPI.sh
+++ sbin/fixModulesAPI.sh
@@ -1,43 +1,30 @@
 #!/bin/bash
-declare -a search
-declare -a replace
-declare -a check
 
-search[0]='function (user|admin)(Action|Index|Popup)\([^)]+\)'
-replace[0]='function \1\2()'
-check[0]='function (user|admin)(Action|Index|Popup)\('
-save[0]=0
+if [ "$1" == "-h" ]; then
+	echo " -h show this page"
+	echo " -rb remove backup files"
+	exit;
+fi
 
-search[1]='\$(app|db|user|request|layout|response|objects|tree)(->|\[)'
-replace[1]='\$this->\1\2'
-check[1]='(\$|\$this->)(app|db|user|request|layout|response|objects|tree)(->|\[)'
-save[1]=0
+for file in `find ./local/modules/ -name '*.module.inc'`
+do
+	if [ "$1" == "-rb" ]; then
+		rm $file.backup;
+	else
+		echo $file
+		echo "created backup file $file.backup"
 
-search[2]='\$currentSid'
-replace[2]='\$this->tree->current->id'
-check[2]='(\$currentSid|\$this->tree->current->id)'
-save[2]=0
+		echo
 
-search[3]='\$this->tree->leafs[$this->tree->current->id]'
-replace[3]='\$this->tree->current'
-check[3]='tree->current'
-save[3]=1
+		cp $file $file.backup
 
-for i in 0 1 2 3
-	do
-		for file in `find ./local/modules/ -name '*.module.inc' | xargs egrep -li "${search[i]}"`
-			do
-				newText=`cat $file | sed -r "s/${search[i]}/${replace[i]}/g"`
+		sed -i -r 's/function\s+(user|admin)(Action|Index|Popup)\([^\)]+\)/function \1\2()/' $file
+		sed -i -r 's/\$(app|db|user|request|layout|response|objects|tree)(->|\[)/$this->\1\2/' $file
+		sed -i -r 's/\$currentSid/$this->tree->current-id/' $file
+		sed -i -r 's/\$this->tree->leafs\[\$this->tree->current->id\]/$this->tree->current/' $file
+	fi;
+done
 
-				if [ ${save[i]} ]
-					then
-						echo "fix $file"
-						echo -e $newText > "$file.fix";
-						mv "$file.fix" $file
-
-					else
-						echo "test $file"
-						echo -e $newText | egrep --color=always "${check[i]}"
-					fi
-			done
-	done
+if [ "$1" != "-rb" ]; then
+	echo "if you want delete backup files use flag -rb"
+fi;
