907ac54464db373988f4fe0b969d625aed41f0af
Mikhail Beloshapka <mikhail.beloshapka@dalee.ru>
2010-10-16 16:02:10 +0000

    Code beautifying

--- tests/unit/run_all.php
+++ tests/unit/run_all.php
@@ -1,13 +1,12 @@
 #!/usr/bin/php5
 <?php
-	/* let it be */
 	define(BASEPATH, realpath(dirname(__FILE__) . '/../../../')."/");
 
 	require_once BASEPATH . '/libpp/lib/mainuser.inc';
 	require_once BASEPATH . '/libpp/lib/mainadmin.inc';
 	require_once BASEPATH . '/libpp/lib/maincommon.inc';
 	
-	#require_once BASEPATH . '/libpp/vendor/simpletest/autorun.php';
+	// require_once BASEPATH . '/libpp/vendor/simpletest/autorun.php';
 	require_once BASEPATH . '/libpp/vendor/simpletest/unit_tester.php';
 	require_once BASEPATH . '/libpp/vendor/simpletest/reporter.php';
 	require_once BASEPATH . '/libpp/vendor/simpletest/collector.php';
@@ -17,13 +16,12 @@
 	$unit_tests_dir = BASEPATH . '/libpp/tests/unit';
 
 	$tests = new TestSuite('libpp tests');
-	
-	$test_single_file = isset($_SERVER['argv'][1]) ? $_SERVER['argv'][1] : NULL;
-    
-    if ($test_single_file)
-        $tests->addFile(realpath($unit_tests_dir . '/' . $test_single_file));
-    else
-	    $tests->collect($unit_tests_dir, new RecursivePatternCollector("#_test.php$#i"));
+
+	if (isset($_SERVER['argv'][1])) {
+		$tests->addFile(realpath($unit_tests_dir . '/' . $_SERVER['argv'][1]));
+	} else {
+		$tests->collect($unit_tests_dir, new RecursivePatternCollector("/_test.php$/i"));
+	}
 
 	$tests->run(new TextReporter());
 ?>
