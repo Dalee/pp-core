6ba4def3af11b441126b3979feecf40b107e55d4
Mikhail Beloshapka <mikhail.beloshapka@dalee.ru>
2010-10-16 16:02:50 +0000

    objectrule added

--- tests/unit/Acl/acl_test.php
+++ tests/unit/Acl/acl_test.php
@@ -6,8 +6,8 @@ class AclTest extends UnitTestCase {
 		$e = new PXEngineSbin();
 		$this->acl = new PXObjectsACL(PXRegistry::getDB(), PXRegistry::getUser());
 	
-		$sql  = "insert into acl_objects (objecttype, what, access) values ('article', 'test_add', 'deny');\n";
-		$sql .= "insert into acl_objects (objecttype, what, access) values ('article', 'test_delete', 'allo');\n";
+		$sql  = "insert into acl_objects (objecttype, what, access, objectrule) values ('article', 'test_add',    'deny', 'user');\n";
+		$sql .= "insert into acl_objects (objecttype, what, access, objectrule) values ('article', 'test_delete', 'allo', 'user');\n";
 
 		PXRegistry::getDB()->modifyingQuery($sql);
 	}
@@ -18,24 +18,28 @@ class AclTest extends UnitTestCase {
 	}
 	
 	function testCheck() {
+
+		// Проверяем работоспособность при отключенном ACL
 		$this->acl->reload();
-		$this->acl->aclEnabled  = false;
-		$r = $this->acl->check('delete', null, null);
-		
-		$this->assertTrue($r);
-		
+		$this->acl->aclEnabled = false;
+
+		$this->assertTrue($this->acl->check('delete', null, null));
+		$this->assertTrue($this->acl->check('add', PXRegistry::getApp()->types['article'], array('id' => 20, 'parent' => 40)));
+
+		// Проверяем работоспособность при включенном ACL
 		$this->acl->aclEnabled  = true;
 		$this->acl->reload();
 
-		$r = $this->acl->check('delete', new PXTypeDescription , array('id'=>null, 'parent'=>null));
+		$r = $this->acl->check('delete', new PXTypeDescription, array('id'=>null, 'parent'=>null));
 		$this->assertFalse($r);
 
+		// $r = $this->acl->check('test_add', 'article', array('id'=>null, 'parent'=>null));
+		// $this->assertFalse($r);
+
 		$type = PXRegistry::getApp()->types['article'];
-		$r = $this->acl->check('test_add', 'article', array('id'=>null, 'parent'=>null));
-		$this->assertFalse($r);
-		
 		$r = $this->acl->check('test_delete', $type, array('id'=>null, 'parent'=>null));
 		$this->assertTrue($r);
+
 	}
 }
 
