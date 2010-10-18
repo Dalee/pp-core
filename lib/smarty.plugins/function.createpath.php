a19c49caf6c966492f169b4bd9be8f624a0a2683
Arkady Eemelyanov <arkady.emelyanov@dalee.ru>
2010-10-18 12:25:36 +0000

    createpath layout trigger, multiregion by uri with path fix

--- lib/smarty.plugins/function.createpath.php
+++ lib/smarty.plugins/function.createpath.php
@@ -1,7 +1,14 @@
 <?php
 function smarty_function_createpath($params, &$smarty) {
+	$createdPath = "";
 	if($params['tree'] && $params['id']) {
-		return createPathByParentId($params['tree'], $params['id']);
+		$createdPath = createPathByParentId($params['tree'], $params['id']);
 	}
+	
+	$app = PXRegistry::getApp();
+	foreach($app->triggers->layout as $t) {
+		$createdPath = $t->getTrigger()->OnAfterPathCreated($createdPath);
+	}
+	return $createdPath;
 }
 ?>
