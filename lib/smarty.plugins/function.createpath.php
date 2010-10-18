803cb96551fb65686f3bf2048bcd9caf83fe42b3
Arkady Eemelyanov <arkady.emelyanov@dalee.ru>
2010-10-18 12:52:31 +0000

    extracted method getUserRidByHost from getUserRid, createpath trigger moved to global function

--- lib/smarty.plugins/function.createpath.php
+++ lib/smarty.plugins/function.createpath.php
@@ -1,14 +1,7 @@
 <?php
 function smarty_function_createpath($params, &$smarty) {
-	$createdPath = "";
 	if($params['tree'] && $params['id']) {
-		$createdPath = createPathByParentId($params['tree'], $params['id']);
+		return createPathByParentId($params['tree'], $params['id']);
 	}
-	
-	$app = PXRegistry::getApp();
-	foreach($app->triggers->layout as $t) {
-		$createdPath = $t->getTrigger()->OnAfterPathCreated($createdPath);
-	}
-	return $createdPath;
 }
 ?>
