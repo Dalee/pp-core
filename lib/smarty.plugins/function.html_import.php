1690c7d4bd5329395f49feb089f644f18c331c4a
Alexey Loktionov <alexey.loktionov@dalee.ru>
2011-06-16 18:05:17 +0000

    add RoR-like behaviour tag html_import

---
+++ lib/smarty.plugins/function.html_import.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Display tag for inclusion local style or javescript files with asset id modifier to manipulate client caching
+ *
+ * Params:
+ * - tag (string) - type of tag
+ * - src (string) - path to local file
+ */
+function smarty_function_html_import($params, &$smarty) {
+	#TODO: add ability to set custom attributes like <link rel="stylesheet" media= ...
+	$tags = array(
+		'css' => '<link rel="stylesheet" type="text/css" href="%s" />',
+		'script' => '<script type="text/javascript" src="%s"></script>'
+	);
+	
+	if(empty($params['src']) || empty($tags[$params['tag']])) {
+		return;
+	}
+	
+	$asset_id = null;
+	
+	foreach(array('/site/htdocs/', '/local/htdocs/', '/libpp/etc/') as $localpath) {
+		if(file_exists($localfile = BASEPATH . $localpath . $params['src'])) {
+			$asset_id = sprintf('?%s=%1$s', filemtime($localfile));
+			break;
+		}
+	}
+	
+	if (empty($asset_id)) {
+		return;
+	}
+
+	printf($tags[$params['tag']], $params['src'] . $asset_id);
+}
+
+?>
