8fb945b4cdd0f997fcd5c396c46ea2a7f0a9a126
Dmitry Kopytine <dmitry.kopytine@dalee.ru>
2011-08-04 16:28:42 +0000

    Extension for the 'filters' plugin: new include policy: more relative paths, searching for additional filters in both 'libpp' and 'local' directories.

--- plugins/filters/decorators.php
+++ plugins/filters/decorators.php
@@ -1,21 +1,21 @@
 <?php
 
 PXDecorativeWidgetsCollection::addToCollection(
-	new PXFiltersAdminWidget,
+	new PXFiltersAdminWidget('filters'),
 	'PXAdminTable',
 	'FILTERS',
 	PXAdminTable::BEFORE_CONTENT_ALWAYS
 );
 
 PXDecorativeWidgetsCollection::addToCollection(
-	new PXOnPageAdminWidget('before'),
+	new PXOnPageAdminWidget('filters', 'before'),
 	'PXAdminPager',
 	'ONPAGE',
 	PXAdminPager::AFTER_PAGE_LIST
 );
 
 PXDecorativeWidgetsCollection::addToCollection(
-	new PXOnPageAdminWidget('after'),
+	new PXOnPageAdminWidget('filters', 'after'),
 	'PXAdminPager',
 	'ONPAGE',
 	PXAdminPager::AFTER_PAGE_LIST
