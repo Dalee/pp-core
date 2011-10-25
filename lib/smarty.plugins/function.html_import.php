532b68bbfa662fa6517497f291d00fa7a533ad2d
Alexey Loktionov <alexey.loktionov@dalee.ru>
2011-10-25 20:42:46 +0000

    urgent hotfix

--- lib/smarty.plugins/function.html_import.php
+++ lib/smarty.plugins/function.html_import.php
@@ -10,15 +10,17 @@
  */
 function smarty_function_html_import($params, &$smarty) {
 	$tags = array(
-		'tag' => '<link rel="stylesheet" type="text/css" href="%s" %s />',
+		'css'    => '<link rel="stylesheet" type="text/css" href="%s" %s />',
 		'script' => '<script type="text/javascript" src="%s" %s></script>'
 	);
+
 	if(empty($params['src']) || empty($tags[$params['tag']])) {
 		return;
 	}
 
 	$extra_params = array();
 	$extra_attributes = "";
+
 	foreach($params as $param => $value){
 		switch($param){
 			case 'tag':
@@ -28,6 +30,7 @@ function smarty_function_html_import($params, &$smarty) {
 				$extra_params[] = sprintf(' %s="%s" ', $param, $value);
 		}
 	}
+
 	if(sizeof($extra_params) != 0){
 		$extra_attributes = implode(' ', $extra_params);
 	}
@@ -35,7 +38,7 @@ function smarty_function_html_import($params, &$smarty) {
 	$asset_id = '';
 	
 	if (strpos($params['src'], 'http') !== 0) {
-		foreach(array('/site/htdocs/', '/local/htdocs/', '/libpp/etc/') as $localpath) {
+		foreach(array('/site/htdocs/', '/local/htdocs/', '/libpp/htdocs/') as $localpath) {
 			if(file_exists($localfile = BASEPATH . $localpath . $params['src'])) {
 				$asset_id = sprintf('?%s=%1$s', filemtime($localfile));
 				break;
