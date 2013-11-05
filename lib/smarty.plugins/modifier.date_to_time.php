5a5f65ec3c9fa6958ad65618156fe8c68cfe2087
Arkady Emelyanov <arkady.emelyanov@dalee.ru>
2013-11-05 15:56:26 +0400

    preg_* calls are now utf8-ready

--- lib/smarty.plugins/modifier.date_to_time.php
+++ lib/smarty.plugins/modifier.date_to_time.php
@@ -8,7 +8,7 @@ function smarty_modifier_date_to_time($string) {
 		return mktime(0,0,0,date('n'),1);
 
 	} elseif ($string != '') {
-		preg_match("/^(\d{2})\.(\d{2})\.(\d{4})\s+(\d{2}):(\d{2}):(\d{2})$/si", trim($string), $date);
+		preg_match("/^(\d{2})\.(\d{2})\.(\d{4})\s+(\d{2}):(\d{2}):(\d{2})$/si".REGEX_MOD, trim($string), $date);
 
 		return mktime($date[4], $date[5], $date[6], $date[2], $date[1], $date[3]);
 
