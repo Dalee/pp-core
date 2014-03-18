41f5552b337bbf0a3a4aa0a84aa0a63154a75abc
Alexey Loktionov <la@dalee.ru>
2014-03-18 20:17:19 +0400

    at least createpath can assign to variable

--- lib/smarty.plugins/function.createpath.php
+++ lib/smarty.plugins/function.createpath.php
@@ -1,7 +1,12 @@
 <?php
 function smarty_function_createpath($params, &$smarty) {
 	if($params['tree'] && $params['id']) {
-		return createPathByParentId($params['tree'], $params['id']);
+		$path = createPathByParentId($params['tree'], $params['id']);
+		if (empty($params['assign'])) {
+			return $path;
+		}
+		
+		$smarty->assign($params['assign'], $path);
 	}
 }
 ?>
