e7b5e68931abd13d5e142dddc9f279c12f38b9fc
Arkady Emelyanov <arkady.emelyanov@dalee.ru>
2017-07-12 20:14:39 +0300

    prepare release of 1.10.0

--- src/Lib/Cache/Driver/Memcached.php
+++ src/Lib/Cache/Driver/Memcached.php
@@ -8,103 +8,122 @@ use PP\Lib\Cache\CacheInterface;
  * Class Memcached
  * @package PP\Lib\Cache\Driver
  */
-class Memcached implements CacheInterface  {
-	private $_mcHandler;
-	private $expirationTime;
+class Memcached implements CacheInterface {
+
+	/** @var \Memcached */
+	private $connection;
+
+	/** @var int */
+	private $defaultExpireTime;
+
+	/** @var string */
 	private $cacheNamespace;
 
-	public  $host = 'localhost';
-	public  $port = 11211;
+	/** @var string */
+	public $host = 'localhost';
+
+	/** @var int */
+	public $port = 11211;
 
 	function __construct($cacheDomain = null, $defaultExpire = 3600, $connectorArgs = null) {
-		extension_loaded("memcached") or FatalError(get_class($this) . " error: memcached extension doesn't loaded or installed");
-		$this->expirationTime = (int)$defaultExpire;
-		$this->cacheNamespace = md5(BASEPATH . $cacheDomain);
-		@list($host, $port)   = explode(':', $connectorArgs, 2);
-		$this->host           = mb_strlen($host) ? $host : $this->host;
-		$this->port           = ($port = (int)$port) > 0 ? $port : $this->port;
-		$this->_mcHandler     = $this->connect();
-	}
-
-	private function connect() {
-		//Avoid persistent connections from cronruns! Non-threadsafe!
-		$mcObject = new \Memcached($this->cacheNamespace . getmypid()); //threadsafe cronjob protection
-
-		if (!count($mcObject->getServerList())) {
-			//Persistent connection settings must be set only once!
-			$mcObject->setOptions(
-				array(
-					\Memcached::OPT_HASH => \Memcached::HASH_MURMUR,
-					\Memcached::OPT_BINARY_PROTOCOL => true,
-					\Memcached::OPT_PREFIX_KEY => $this->cacheNamespace,
-					\Memcached::OPT_TCP_NODELAY => true //for small data packets
-				)
-			);
-
-			//TODO: multiple servers support NNADA?!
-			$mcObject->addServer($this->host, $this->port) or FatalError(
-				get_class($this) . " error: could not add Memcached server at {$this->host}:{$this->port} !"
-			);
+		if (!extension_loaded("memcached")) {
+			throw new \Exception("Memcached extension is not loaded");
 		}
 
-		return $mcObject;
+		$this->defaultExpireTime = (int)$defaultExpire;
+		$this->cacheNamespace = md5(BASEPATH . $cacheDomain);
+		$this->host = getFromArray($connectorArgs, 'host', $this->host);
+		$this->port = (int)getFromArray($connectorArgs, 'port', $this->port);
+		$this->connection = $this->connect();
 	}
 
-	function exists($key) {
-		$this->_mcHandler->get($this->key($key));
-		return $this->_mcHandler->getResultCode() !== \Memcached::RES_NOTFOUND;
+	public function exists($key) {
+		$this->connection->get($this->key($key));
+		return $this->connection->getResultCode() !== \Memcached::RES_NOTFOUND;
 	}
 
-	function save($key, $data, $expTime = null) {
+	public function save($key, $data, $expTime = null) {
 		$expTime = (int)$expTime;
-		$this->_mcHandler->set($this->key($key), $data, $expTime > 0 ? $expTime : $this->expirationTime);
+		$expTime = ($expTime > 0)
+			? $expTime
+			: $this->defaultExpireTime;
+
+		$this->connection->set($this->key($key), $data, $expTime);
 	}
 
-	function load($key) {
-		$res = $this->_mcHandler->get($this->key($key)); // look at that: https://github.com/php-memcached-dev/php-memcached/issues/21
-		return $this->_mcHandler->getResultCode() == \Memcached::RES_NOTFOUND ? null : $res;
+	public function load($key) {
+		// look at that: https://github.com/php-memcached-dev/php-memcached/issues/21
+		$res = $this->connection->get($this->key($key));
+		return $this->connection->getResultCode() == \Memcached::RES_NOTFOUND ? null : $res;
 	}
 
-	function delete($key) {
-		$this->_mcHandler->delete($this->key($key));
+	public function delete($key) {
+		$this->connection->delete($this->key($key));
 	}
 
-	function deleteGroup($group) {
+	public function deleteGroup($group) {
 		$prefix = $this->key($group, true);
 		$prefLen = mb_strlen($prefix);
-		$allKeys = $this->_mcHandler->getAllKeys();
+		$allKeys = $this->connection->getAllKeys();
 		if (empty($allKeys)) {
 			return;
 		}
+
 		$toDelete = [];
 		foreach ($allKeys as $key) {
 			if (mb_substr($key, 0, $prefLen) == $prefix) {
 				$toDelete[] = $key;
 			}
 		}
-		if (empty($toDelete)) {
-			return;
+
+		if (!empty($toDelete)) {
+			$this->connection->deleteMulti($toDelete);
 		}
-		$this->_mcHandler->deleteMulti($toDelete);
 	}
 
-	function increment($key, $offset = 1 , $initial = 0, $expTime = null) {
-		($expTime > 0) || ($expTime = $this->expirationTime);
+	public function increment($key, $offset = 1, $initial = 0, $expTime = null) {
+		$expTime = (int)$expTime;
+		$expTime = ($expTime > 0)
+			? $expTime
+			: $this->defaultExpireTime;
+
 		$k = $this->key($key);
-		$this->_mcHandler->add($k, $initial, $expTime);
-		return $this->_mcHandler->increment($this->key($key), $offset, $initial, $expTime);
+		$this->connection->add($k, $initial, $expTime);
+		return $this->connection->increment($this->key($key), $offset, $initial, $expTime);
+	}
+
+	public function clear() {
+		$this->connection->flush();
+	}
+
+	private function connect() {
+		// WARNING: Avoid persistent connections from cron tasks: task runner uses fork
+		// emulate persistent connection_id.
+		$mcObject = new \Memcached($this->cacheNamespace . getmypid());
+
+		if (!count($mcObject->getServerList())) {
+			// WARNING: persistent connection settings must be set only once!
+			$mcObject->setOptions([
+				\Memcached::OPT_HASH => \Memcached::HASH_MURMUR,
+				\Memcached::OPT_BINARY_PROTOCOL => true,
+				\Memcached::OPT_PREFIX_KEY => $this->cacheNamespace,
+				\Memcached::OPT_TCP_NODELAY => true //for small data packets
+			]);
+
+			if (!$mcObject->addServer($this->host, $this->port)) {
+				throw new \Exception("Connection to memcached: {$this->host}:{$this->port} failed");
+			}
+		}
+
+		return $mcObject;
 	}
 
 	private function key($key, $glob = false) {
 		if (is_array($key)) {
-			$keyPart   = $this->key(array_shift($key));
+			$keyPart = $this->key(array_shift($key));
 			$groupPart = $this->key(array_shift($key));
 			return $groupPart . '_' . $keyPart;
 		}
 		return md5($key) . ($glob ? '_' : '');
-	}
-
-	function clear() {
-		$this->_mcHandler->flush();
 	}
 }
